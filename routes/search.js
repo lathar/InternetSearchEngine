0adff6b4aa449a4462b319a567e7d55dd74969d6
Ramasamy, r.latha(AWF) <lramasamy@paypal.com>
2015-01-30 10:09:37 +0530

    Added Bootstrap

--- routes/search.js
+++ routes/search.js
@@ -2,16 +2,13 @@ var request = require('request');
 
 exports.searchDetails = function (req,res) {
 
-	// console.log('~~~~~~~~~', req.body.search);
-	req.model = {
+		req.model = {
         	data : {} 
     };
 	request('http://api.duckduckgo.com/?q='+req.body.search+'&format=json&pretty=0', function (error, response, body) {
-		if (!error && response.statusCode == 200) {
-			
-			req.model.data = JSON.parse(body);
-    		// console.log(req.model.data);
-			res.render('search', req.model);
-		}
+	if (!error && response.statusCode == 200) {
+		req.model.data = JSON.parse(body);
+		res.render('search', req.model);
+	 	}
 	});
 }
